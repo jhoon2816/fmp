dd66969f5238b513fe9a75debc7ece2b1bd0c6f6
jhoon2816 <jhoon2816@gmail.com>
2019-08-07 17:45:43 +0900

    passport strategy 구현

--- passport/index.js
+++ passport/index.js
@@ -1,20 +1,19 @@
 const local = require('./localStrategy');
 const kakao = require('./kakaoStrategy');
-// const google = require('./googleStrategy');
+const google = require('./googleStrategy');
 
 const { User } = require('../models');
 
 module.exports = (passport) => {
+
     passport.serializeUser((user,done)=>{
        done(null, user.id);
     });
-
     passport.deserializeUser((id, done)=>{
        User.find({ where: { id } })
            .then(user => done(null,user))
            .catch(err => done(err));
     });
-
     local(passport);
     kakao(passport);
 };
